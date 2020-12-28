4b07e2aadd9842424aa149265de79d645d5dc604
dmk940 <72927014+dmk940@users.noreply.github.com>
2020-12-28 22:31:40 +0000

    Change to new synth pop game audio
    (License free, free use audio)

--- core/src/main/java/com/maingame/game/MainGame.java
+++ core/src/main/java/com/maingame/game/MainGame.java
@@ -25,7 +25,7 @@ public class MainGame extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		gsm = new GameStateManager();
-		music = Gdx.audio.newMusic(Gdx.files.internal("game3.mp3"));
+		music = Gdx.audio.newMusic(Gdx.files.internal("synth_pop_game.mp3"));
 		music.setLooping(true);
 		music.setVolume(0.1f);
 		music.play();
